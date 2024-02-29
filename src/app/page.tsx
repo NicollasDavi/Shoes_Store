e9c70481f551c2550592da7b820fd33b876cdb2e
NicollasDavi <132391248+NicollasDavi@users.noreply.github.com>
2024-02-28 22:32:14 -0300

    Up Settings

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Nav from "./Navigation/Nav";
+import ProductPage from "./pages/ProductPage";
+
+export default function Home() {
+  return (
+    <main>
+      <Nav />
+      <ProductPage />
+    </main>
+  );
+}
